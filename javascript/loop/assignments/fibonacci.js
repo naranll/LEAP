614efc7cfb9db442d15c51437262ecbf9c731484
Morning <morning@admins-iMac.local>
2022-12-01 12:37:09 +0800

    modified

--- javascript/loop/assignments/fibonacci.js
+++ javascript/loop/assignments/fibonacci.js
@@ -1,10 +1,13 @@
 let n = parseFloat(prompt("Enter a number"));
-let a = 1, b = 1, c;
+let a = 1, b = 1, c, i=1;
 
-while (n <= 10) {   
+//fic output method
+
+while (i <= n) {   
     c = a + b;
+    console.log(c);
     alert(c);
     a = b;
     b = c;
-    n++;
+    i++;
 }
