07290364107fec9919f16144295a283654b2aded
Morning <morning@admins-iMac.local>
2022-12-07 13:36:07 +0800

    methods

---
+++ javascript/methods/assignment/palindromic.js
@@ -0,0 +1,33 @@
+let verse = "abc cba";
+
+// let chars = verse.split('');
+// let reversed = (verse.split('')).reverse();
+// console.log(chars);
+// console.log(reversed);
+
+// if (chars[0] == reversed[0] && chars.length != 1) {
+//     chars.shift();
+//     reversed.shift();
+//     console.log("yes");
+// } 
+// console.log(chars);
+// console.log(reversed);
+
+function check(word) {
+    let letters = word.split('');
+    let reversed = (word.split('')).reverse();
+    // console.log(letters);
+    // console.log(reversed);
+    cut(letters, reversed);
+    console.log("yes");
+}
+
+function cut(l,r) {
+    if (l[0] == r[0] && l.length > 0) {
+        l = l.slice(1, l.length);
+        r = r.slice(1, r.length);
+        cut(l,r);
+    }
+}
+
+check(verse);
---
+++ javascript/methods/classwork/methods.js
@@ -0,0 +1,73 @@
+let array = ["Red", "Orange", "Yellow", "Green", "Blue", "Navy", "Purple"];
+
+//AT
+console.log(array.at(3));
+
+//CONCAT
+let arr2 = ["Pink", "White", "Black"];
+let arr3 = array.concat(arr2);
+console.log(arr3);
+
+//INCLUDES
+console.log(array.includes("Black" + "\n"));
+
+//INDEXOF
+console.log(array.indexOf("Orange"));
+console.log(array.indexOf("Yellow", 3)+ "\n");    //after array[3]
+
+//JOIN
+console.log(array.join() + "\n");
+
+//POP
+let popped = array.pop();
+console.log(popped);
+console.log(array + "\n");
+
+//PUSH
+let pushed = array.push("Grey");
+console.log(pushed);
+console.log(array + "\n");
+
+//SHIFT
+let shifted = array.shift();
+console.log(shifted);
+console.log(array + "\n");
+
+//UNSHIFT 
+let unsh = array.unshift("Red");
+console.log(unsh);
+console.log(array + "\n");
+
+//SORT
+console.log(array.sort() + "\n");
+
+//REVERSE
+console.log(array.reverse() + "\n");
+
+//SLICE 
+console.log(array.slice(1,5) + "\n");
+
+//SPLICE (STARTindex, DELETEcount, ITEM1, ITEM2, ITEMN)
+array.splice(0, 0, "Brown");
+console.log(array + "\n");
+
+let arr4 = arr2.splice(0, 1 ,'Brown');
+console.log(arr2);
+console.log(arr4 + "\n");
+
+//STRING METHODS
+//SPLIT
+let sent = "The big brown fox jumps over the lazy dog";
+console.log(sent); 
+console.log(sent.split(' ') + "\n"); //there is a space between ' '  = split by spaces
+
+const chara = sent.split('');   //there is no space netween '' = split by characters
+console.log(chara);
+console.log(chara[1]);
+
+//if there is nothing in split parenthesis, then nothing splits
+
+//REPLACE & REPLACEALL
+console.log(sent.replace("o", "Z"));
+console.log(sent.replaceAll("o", "Z" ));
+
