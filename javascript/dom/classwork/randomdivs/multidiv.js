f43ba6c7c522562c76f3f48bf390ec27c590bee8
naranll <morning@admins-iMac-7.local>
2023-02-09 10:45:59 +0800

    mods

--- javascript/dom/classwork/randomdivs/multidiv.js
+++ javascript/dom/classwork/randomdivs/multidiv.js
@@ -2,17 +2,26 @@ let rootdiv = document.querySelector("#root");
 rootdiv.style.display = "flex";
 rootdiv.style.flexWrap = "wrap";
 
-function randomNum(min, max){
-    let r = Math.floor((Math.random() * (max-min) +1));
+function randomNum(min, max) {
+    let r = Math.floor((Math.random() * (max - min) + 1));
     return r;
 }
 
+// for (let i = 0; i < 16; i++) {
+//     let newdivs = document.createElement("div");
+//     newdivs.style.border = "1px solid brown";
+//     newdivs.style.width = "150px";
+//     newdivs.style.height = "200px";
+//     newdivs.style.backgroundColor = `rgb(${randomNum(0,225)},${randomNum(0,225)},${randomNum(0,225)})`;
+
+//     rootdiv.appendChild(newdivs);
+// }
+
 for (let i = 0; i < 16; i++) {
     let newdivs = document.createElement("div");
-    newdivs.style.border = "1px solid brown";
-    newdivs.style.width = "150px";
-    newdivs.style.height = "200px";
-    newdivs.style.backgroundColor = `rgb(${randomNum(0,225)},${randomNum(0,225)},${randomNum(0,225)})`;
-
+    newdivs.style.border = "1px solid black";
+    newdivs.style.width = "100px";
+    newdivs.style.height = "500px";
+    newdivs.style.backgroundColor = `rgb(${randomNum(0, 220)}, ${randomNum(0, 220)}, ${randomNum(0, 220)} )`
     rootdiv.appendChild(newdivs);
 }
